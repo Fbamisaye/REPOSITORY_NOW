3c869f76cd5fcf84d7bd406e6bc85ba030302b09
FOLAKE <fola.bamisaye@gmail.com>
2019-05-03 03:22:17 +0100

    Added

---
+++ AbstractClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolaBami2019Sol
+{
+  public  abstract class AbstractClass
+
+  {
+      private int paramOne;
+      private int paramTwo;
+      protected double addResult;
+
+      public void AdditionResult()
+      {
+          Console.WriteLine("Addition of {0} and {1} is: {2}", paramOne, paramTwo, addResult);
+      }
+
+      public abstract void AddMe(int numOne, int numTwo);
+  }
+}
---
+++ Grandkid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolaBami2019Sol
+{
+    public class Grandkid : ChildAbstract
+    {
+        public override void AddMe(int numOne, double numTwo)
+        {
+            addResult = numOne + numTwo;
+
+        }
+    }
+}
